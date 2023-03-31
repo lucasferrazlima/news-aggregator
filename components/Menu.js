b1359ef5939944a31a1eeca11efc42e9f1a36f19
Lucas <lucasferrazl@hotmail.com>
2023-03-31 11:11:22 -0300

    Modify Card structure to include every relevant info

---
+++ components/Menu.js
@@ -0,0 +1,7 @@
+export default function Menu() {
+  return (
+    <ul>
+      <li></li>
+    </ul>
+  )
+}
--- components/NewsCard.js
+++ components/NewsCard.js
@@ -6,12 +6,14 @@ const Feed = styled.div`
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   grid-gap: 3rem;
   padding: 1rem;
+  padding-left: 10%;
+  padding-right: 10rem;
 `
 
 const Card = styled.div`
   width: 100%;
   height: auto;
-  background-color: lightblue;
+  background-color: lightblue; 
 `
 const ArticleImage = styled.img`
   width: 100%;
@@ -21,12 +23,24 @@ const ArticleImage = styled.img`
 export default function NewsCard({ articles }) {
   return (
     <Feed>
-      {articles.map((article) => (
-        <Card key={article.url}>
-          <p>{article.title}</p>
-            <ArticleImage src={article.urlToImage} alt={article.description} />
+      {articles.map((article) => {
+        
+        // Remove the source name from the title
+        const articleTitle = article.title.replace(` - ${article.source.name}`, '')
+
+        // Format the published date
+        const publishedDate = new Date()
+
+        return(
+          <Card key={article.url}>
+          <ArticleImage src={article.urlToImage} alt={article.description} />
+          <div>{article.source.name}</div>
+          <div>{publishedDate.toLocaleDateString()}</div>
+          <p>{articleTitle}</p>
+          <a href={article.url} target="_blank">Read full article</a>
         </Card>
-      ))}
+        )
+      })}
     </Feed>
   )
 }
