8b87b914b1cf2ce27509b9ac9aeeb990b4aa9ece
Lucas <lucasferrazl@hotmail.com>
2023-03-31 10:02:52 -0300

    Add grid layout to main feed

--- components/NewsCard.js
+++ components/NewsCard.js
@@ -1,10 +1,32 @@
+import Image from 'next/image'
+import styled from 'styled-components'
+
+const Feed = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 3rem;
+  padding: 1rem;
+`
+
+const Card = styled.div`
+  width: 100%;
+  height: auto;
+  background-color: lightblue;
+`
+const ArticleImage = styled.img`
+  width: 100%;
+  height: auto;
+`
+
 export default function NewsCard({ articles }) {
-  return(
-    <>
-    {articles.map(article => {
-      return <p key={article.url}>{article.title}</p>
-    })
-    }
-    </>
+  return (
+    <Feed>
+      {articles.map((article) => (
+        <Card key={article.url}>
+          <p>{article.title}</p>
+            <ArticleImage src={article.urlToImage} alt={article.description} />
+        </Card>
+      ))}
+    </Feed>
   )
 }
