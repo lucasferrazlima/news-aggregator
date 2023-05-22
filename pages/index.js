99fac1ce030212166d7a938c9f04310d1ffd3ad9
Lucas <lucasferrazl@hotmail.com>
2023-05-22 18:41:18 -0300

    Update fetching on index page

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Menu from '../components/Menu'
 import { category } from '../components/Menu'
 
 // Fetching data from NewsAPI.org
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const apiKey = process.env.NEWSAPI_KEY
   let url = `https://newsapi.org/v2/top-headlines?country=us&Categogry='General'&pageSize=100&apiKey=${apiKey}`
   const response = await fetch(url)
